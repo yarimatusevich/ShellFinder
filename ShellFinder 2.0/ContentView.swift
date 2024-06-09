6737db5ce551a234a156f351b2db502af5ec5d62
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-06-08 22:52:48 -0400

    Added text

--- ShellFinder 2.0/ContentView.swift
+++ ShellFinder 2.0/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+                
+            Text("Hello visconti!")
         }
         .padding()
     }
