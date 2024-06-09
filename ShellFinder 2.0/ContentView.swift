fb8755ebce6a2ef765ec188bac06b1cd0381686c
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-06-08 21:23:26 -0400

    Removed text

--- ShellFinder 2.0/ContentView.swift
+++ ShellFinder 2.0/ContentView.swift
@@ -13,7 +13,6 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, this is my first commit!")
         }
         .padding()
     }
