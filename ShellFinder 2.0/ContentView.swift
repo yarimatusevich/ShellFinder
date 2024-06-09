61436e9f121500c7253b20c258b9627af6ae1a1d
Drew Visconti <andrewvisconti915@gmail.com>
2024-06-08 23:03:00 -0400

    hi

--- ShellFinder 2.0/ContentView.swift
+++ ShellFinder 2.0/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
                 
-            Text("Hello visconti!")
+            Text("Hello bunsen :)")
         }
         .padding()
     }
