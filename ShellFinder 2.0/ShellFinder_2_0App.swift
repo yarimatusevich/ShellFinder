e2a64b010ca10f32ca1e2fc9d2301ea3cd941a2e
Yaroslav Matusevich <yari.matusevich@bmail.com>
2024-06-08 21:07:08 -0400

    Initial Commit

---
+++ ShellFinder 2.0/ShellFinder_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ShellFinder_2_0App.swift
+//  ShellFinder 2.0
+//
+//  Created by Yaroslav Matusevich on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ShellFinder_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
