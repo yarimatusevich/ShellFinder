6b278daf77fa6fadbb524e1f191c413577ee86a7
Drew Visconti <andrewvisconti915@gmail.com>
2024-06-10 15:36:48 -0400

    Made TabView system and placed capture button above the TabView. (TABVIEW NEEDS SEPARATE VIEWS)

--- ShellFinder 2.0/ContentView.swift
+++ ShellFinder 2.0/ContentView.swift
@@ -1,13 +1,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedTab = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        
+        ZStack {
+            TabView(selection: $selectedTab) {
+                
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .tabItem {
+                        Image(systemName: "fossil.shell")
+                        Text("My Shells")
+                    }
+                    .tag(0)
+                
+                Text("hello vro..")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Profile")
+                    }
+                    .tag(1)
+
+            }
+            
+            Button(action: {
+                        // Define button action here
+                    }) {
+                        Image(systemName: "camera")
+                            .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(.white)
+                                .padding(20)
+                                .background(Circle().fill(Color.blue).frame(width: 70, height: 70))
+                                .shadow(radius: 5)
+                    }
+                    .offset(y: 325)
+            
         }
-        .padding()
     }
 }
 
