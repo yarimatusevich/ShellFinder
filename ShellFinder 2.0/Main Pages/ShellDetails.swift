383d09dd5f6b16c0211987c6d82ee713d4df0b4b
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-06-13 22:40:51 -0400

    Fixed images on discover page

---
+++ ShellFinder 2.0/Main Pages/ShellDetails.swift
@@ -0,0 +1,18 @@
+//
+//  ShellDetails.swift
+//  ShellFinder 2.0
+//
+//  Created by Yaroslav Matusevich on 6/13/24.
+//
+
+import SwiftUI
+
+struct ShellDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShellDetails()
+}
