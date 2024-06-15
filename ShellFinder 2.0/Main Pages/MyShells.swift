5b3d3ce3de979954e3575961b91ddf90fcd2e303
Drew Visconti <andrewvisconti915@gmail.com>
2024-06-15 03:01:39 -0400

    Added functionality to the My Shells page

--- ShellFinder 2.0/Main Pages/MyShells.swift
+++ ShellFinder 2.0/Main Pages/MyShells.swift
@@ -1,31 +1,56 @@
 import SwiftUI
 
 struct MyShells: View {
+    
+    @State private var buttonCount = 2
+    
     var body: some View {
         
         TabView {
             NavigationView {
-                ScrollView {
-                    Text("Hello World")
-                    Text("Hello World")
-                    Text("Hello World")
+                
+                List(0..<buttonCount, id: \.self) { _ in
+                    NavigationLink(destination: Text("Hello!!")) {
+                        HStack {
+                            Image("channeled_whelk")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 145, height: 130, alignment: .center)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .padding(.trailing, 5)
+                            
+                            VStack(alignment: .leading) {
+                                
+                                Text("TEST")
+                                    .font(.system(size: 16))
+                                    .lineSpacing(5)
+                                    .padding(.bottom, 5)
+                                Text("testingg")
+                                    .font(.system(size: 15))
+                                    .italic()
+                                    .lineSpacing(5)
+                            }
+                        }
+                    }
+                    .navigationTitle("My Shells")
+                    .navigationBarItems(
+                        leading: NavigationLink (
+                            destination: Text("Destination"),
+                            label: {
+                                Text("Favorites")
+                            }),
+                        trailing: NavigationLink (
+                            destination: Button(action: {buttonCount += 1; print(buttonCount)}, label: {
+                                Text("Button")
+                            }),
+                            label: {
+                                Text("Navigate")
+                            }))
                 }
-                .navigationTitle("My Shells")
-                .navigationBarItems(
-                    leading: NavigationLink (
-                        destination: Text("Destination"),
-                          label: {
-                              Text("Favorites")
-                          }),
-                    trailing: NavigationLink (
-                        destination: Text("Destination"),
-                          label: {
-                              Text("Navigate")
-                          }))
             }
         }
     }
 }
-#Preview {
-    MyShells()
-}
+//#Preview {
+//    MyShells()
+//}
