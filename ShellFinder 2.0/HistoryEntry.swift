38eb9325c929aea14ee7b59ea1e82d6f1413264d
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-06-16 22:22:08 -0400

    Created HistoryEntry class, made MyShells modular, created test function

---
+++ ShellFinder 2.0/HistoryEntry.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryEntry.swift
+//  ShellFinder 2.0
+//
+//  Created by Yaroslav Matusevich on 6/16/24.
+//
+
+import Foundation
---
+++ ShellFinder 2.0/MyShellsModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyShellsModel.swift
+//  ShellFinder 2.0
+//
+//  Created by Yaroslav Matusevich on 6/16/24.
+//
+
+import Foundation
