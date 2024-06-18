114c6ec566369a3b7f7348a41eef31dbcc474000
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-06-18 18:37:33 -0400

    Created User class

--- ShellFinder 2.0/User.swift
+++ ShellFinder 2.0/User.swift
@@ -1,8 +1,42 @@
-//
-//  User.swift
-//  ShellFinder 2.0
-//
-//  Created by Yaroslav Matusevich on 6/18/24.
-//
-
 import Foundation
+
+// Going to have to create some sort of user database
+// Will research
+
+class User {
+    private let name: String
+    private let id: String // user name, unique to every user
+    private var password: String
+    private var numberOfIdenifications: Int // Number of identifications
+    
+    init(name: String, id: String, password: String, numberOfIdenifications: Int) {
+        self.name = name
+        self.id = id
+        self.password = password
+        self.numberOfIdenifications = numberOfIdenifications
+    }
+    
+    public func getName() -> String {
+        return name
+    }
+    
+    public func getId() -> String {
+        return id
+    }
+    
+    public func getNumberOfIdentifications() -> Int {
+        return numberOfIdenifications
+    }
+    
+    public func getPassword() -> String {
+        return password
+    }
+    
+    public func setPassword(password: String) {
+        self.password = password
+    }
+    
+    public func setNumberOfIdentifications(number: Int) {
+        self.numberOfIdenifications = number
+    }
+}
