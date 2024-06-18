bc52d49c8a4b5ecf7004bd3f218c825b37e1a205
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-06-18 18:35:44 -0400

    Created ProfileModel class and user object

---
+++ ShellFinder 2.0/ProfileModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileModel.swift
+//  ShellFinder 2.0
+//
+//  Created by Yaroslav Matusevich on 6/18/24.
+//
+
+import Foundation
---
+++ ShellFinder 2.0/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  ShellFinder 2.0
+//
+//  Created by Yaroslav Matusevich on 6/18/24.
+//
+
+import Foundation
