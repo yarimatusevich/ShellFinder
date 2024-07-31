4fb74123b86f84e1716ea6ed0898fa60df82baff
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-07-30 20:14:48 -0400

    Deintegrated cocoa pods as it is no longer required

--- Pods/Target Support Files/Pods-ShellFinder 2.0-ShellFinder 2.0UITests/Pods-ShellFinder 2.0-ShellFinder 2.0UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ShellFinder_2_0_ShellFinder_2_0UITests : NSObject
-@end
-@implementation PodsDummy_Pods_ShellFinder_2_0_ShellFinder_2_0UITests
-@end
