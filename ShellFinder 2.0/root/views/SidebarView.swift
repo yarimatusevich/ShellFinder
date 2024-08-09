d3ed494a4d7adce24a3439ff4f4acd883367eb43
Drew Visconti <andrewvisconti915@gmail.com>
2024-08-09 17:38:32 -0400

    Started implementing Hamburger settings menu into the CameraView

---
+++ ShellFinder 2.0/root/views/SidebarView.swift
@@ -0,0 +1,18 @@
+//
+//  SidebarView.swift
+//  ShellFinder 2.0
+//
+//  Created by Drew Visconti on 8/9/24.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SidebarView()
+}
