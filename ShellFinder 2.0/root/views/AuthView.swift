e8cdaec9f95fbe43ca1545f7d7e7b496b06e2461
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-08-09 22:25:53 -0400

    Authorization now works correctly, the UI still needs some work though

--- ShellFinder 2.0/root/views/AuthView.swift
+++ ShellFinder 2.0/root/views/AuthView.swift
@@ -1,31 +1,103 @@
 import SwiftUI
+import FirebaseAuth
 
 struct AuthView: View {
     
     @State var email: String = ""
     @State var password: String = ""
+    @State var displayingLogIn: Bool = false // whether or not showing log in view
+    @State var userIsLoggedIn: Bool = false
     
     var body: some View {
-        VStack{
-            Text("Welcome")
+        if (userIsLoggedIn) {
+            ContentView()
+        } else {
+            content
+        }
+    }
+    
+    var content: some View {
+        VStack {
+            Text("Welcome to ShellFinder")
                 .font(.system(size: 40))
-                .offset(y: -200)
+                .offset(y: -150)
             
-            HStack{
-                Spacer()
+            HStack {
                 TextField("Email", text: $email)
                     .bold()
                     .font(.system(size: 20))
-                Spacer()
+                    .padding(.horizontal, 20)
             }
+            .padding(.vertical, 10)
             
             HStack {
-                Spacer()
                 SecureField("Password", text: $password)
                     .bold()
                     .font(.system(size: 20))
-                Spacer()
+                    .padding(.horizontal, 20)
             }
+            .padding(.vertical, 10)
+            
+            // Sign up button
+            Button {
+                Authentication.registerUser(email: email, password: password)
+            } label: {
+                Text("Sign up")
+            }
+
+            // Log in button
+            Button {
+                displayingLogIn = true
+            } label: {
+                Text("Already have an account? Log in")
+            }
+            // Sheet bringing up prompts for log in info
+            .sheet(isPresented: $displayingLogIn, content: {
+                LoginView()
+            })
+        }
+        // On appear checks if user is already logged in
+        .onAppear {
+            Auth.auth().addStateDidChangeListener { auth, user in
+                if (user != nil) {
+                    userIsLoggedIn.toggle()
+                }
+            }
+        }
+    }
+}
+
+struct LoginView: View {
+    
+    @State var email : String = ""
+    @State var password : String = ""
+    
+    var body: some View {
+        VStack {
+            Text("Welcome back!")
+            
+            HStack {
+                TextField("Email", text: $email)
+                    .bold()
+                    .font(.system(size: 20))
+                    .padding(.horizontal, 20)
+            }
+            .padding(.vertical, 10)
+            
+            HStack {
+                SecureField("Password", text: $password)
+                    .bold()
+                    .font(.system(size: 20))
+                    .padding(.horizontal, 20)
+            }
+            .padding(.vertical, 10)
+            
+            Button {
+                Authentication.logIn(email: email, password: password)
+            } label: {
+                Text("Log in")
+            }
+
         }
     }
 }
