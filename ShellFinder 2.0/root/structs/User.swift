adb3b2a79e528a6f4df1e9020fd33cebfc68b4c7
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-08-09 22:24:10 -0400

    Edited User class

--- ShellFinder 2.0/root/structs/User.swift
+++ ShellFinder 2.0/root/structs/User.swift
@@ -3,12 +3,25 @@ import FirebaseFirestore
 
 struct User: Codable {
     @DocumentID var id: String?
-    private let name: String
+    private let firstName: String
+    private let lastName: String
     private let numberOfIdentifications: Int
+    private let userHistory: Array<HistoryEntry>
     
-    init(id: String? = nil, name: String, numberOfIdentifications: Int) {
+    init(id: String? = nil, firstName: String, lastName: String, numberOfIdentifications: Int, history: Array<HistoryEntry>) {
         self.id = id
-        self.name = name
+        self.firstName = firstName
+        self.lastName = lastName
         self.numberOfIdentifications = numberOfIdentifications
+        self.userHistory = history
     }
+    
+    // Getters
+    public func getFirstName() -> String { return firstName }
+    
+    public func getLastName() -> String { return lastName }
+    
+    public func getNumberOfIdentifications() -> Int { return numberOfIdentifications }
+    
+    public func getUserHistory() -> Array<HistoryEntry> { return userHistory }
 }
