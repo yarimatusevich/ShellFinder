6f238d27169be0edde5ef75f9b9eed4d802717b1
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-08-09 22:26:19 -0400

    Formatting edit

--- ShellFinder 2.0/root/utils/ShellFinder_2_0App.swift
+++ ShellFinder 2.0/root/utils/ShellFinder_2_0App.swift
@@ -9,16 +9,15 @@ struct ShellFinder_2_0App: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AuthView()
         }
     }
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      FirebaseApp.configure()
+      return true
+    }
 }
