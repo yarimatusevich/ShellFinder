0f249f0f75cad501cc7c0949093d5d641eaf52ef
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-08-13 22:20:40 -0400

    Edited spacing

--- ShellFinder 2.0/root/views/SidebarView.swift
+++ ShellFinder 2.0/root/views/SidebarView.swift
@@ -1,10 +1,3 @@
-//
-//  SidebarView.swift
-//  ShellFinder 2.0
-//
-//  Created by Drew Visconti on 8/9/24.
-//
-
 import SwiftUI
 import Foundation
 
@@ -14,6 +7,7 @@ struct SidebarView: View {
     @State var isProfileSheet = false
     
     var body: some View {
+        
         VStack(alignment: .leading) {
             
             Button(action: {
@@ -47,10 +41,10 @@ struct SidebarView: View {
         .background(Color.white)
         .foregroundColor(.black)
         .font(.system(size: 20))
-        
         .sheet(isPresented: $isSettingsSheet, content: {
             Text("Settings")
         })
+        
         .sheet(isPresented: $isProfileSheet, content: {
             Text("About us")
         })
