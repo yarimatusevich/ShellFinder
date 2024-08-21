17a9a40e72d64202db6cee3196d8f3b3a7d0051f
Yaroslav Matusevich <yari.matusevich@gmail.com>
2024-08-21 17:01:53 -0400

    Added TODO comment

--- ShellFinder 2.0/root/utils/ShellFinder_2_0App.swift
+++ ShellFinder 2.0/root/utils/ShellFinder_2_0App.swift
@@ -2,13 +2,13 @@ import SwiftUI
 import FirebaseCore
 
 @main
-
 struct ShellFinder_2_0App: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var network = ShellFinderNetwork() // object handling data from app API
     @StateObject var authentication = ShellFinderAuth() // object handling user authentication
     
+    // TODO: delay view rendering and create loading screen
     var body: some Scene {
         WindowGroup {
             if (authentication.userIsLoggedIn) {
